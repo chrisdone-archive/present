a5ad094a706e3aa339ae02718eaa2ff6c332886f
Chris Done <chrisdone@gmail.com>
2016-03-08 22:21:26 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-stack exec --no-ghc-package-path -- runhaskell Setup.hs configure --global --package-db /Users/chris/.stack/programs/x86_64-osx/ghc-7.10.3/lib/ghc-7.10.3/package.conf.d
+# Installs globally to your GHC's global package database
+
+stack exec --no-ghc-package-path -- runhaskell Setup.hs clean
+stack exec --no-ghc-package-path -- runhaskell Setup.hs configure --global --package-db $(stack path --global-pkg-db)
 stack exec --no-ghc-package-path -- runhaskell Setup.hs build
 stack exec --no-ghc-package-path -- runhaskell Setup.hs install
