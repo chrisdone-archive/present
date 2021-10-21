0d537ba614fb9597d92558979477cdd2f625adce
Chris Done <git@chrisdone.com>
2021-10-21 10:45:27 +0100

    Modernize

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-((haskell-mode
-  . ((haskell-process-type . ghci)
-     (haskell-process-use-ghci . t)
-     (haskell-process-path-ghci . "stack")
-     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng"))))
- (haskell-cabal-mode
-  . ((haskell-process-type . ghci)
-     (haskell-process-path-ghci . "stack")
-     (haskell-process-args-ghci . ("ghci" "--with-ghc" "ghci-ng")))))
